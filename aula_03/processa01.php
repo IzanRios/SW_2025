c089145fa956708813fd66fad41569e960331a23
IzanRios <160190737+IzanRios@users.noreply.github.com>
2025-02-27 08:54:57 -0300

    Add files via upload

---
+++ aula_03/processa01.php
@@ -0,0 +1,9 @@
+<?php
+    $nome = $_POST['cxnome'];
+    $idade = $_POST['cxidade'];
+
+    echo "Nome preenchido: $nome <br>";
+    echo "Idade preenchida: $nome <br>";
+
+
+?>
---
+++ aula_03/processa02.php
@@ -0,0 +1,9 @@
+<?php
+    $nome = $_POST['cxnome'];
+    $mensagem = $_POST['cxmensagem'];
+
+    echo "Nome preenchido: $nome <br>";
+    echo "Mensagem enviada: $mensagem <br>";
+
+
+?>
